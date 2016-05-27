5c5b9bbb488ea8c46786d27992c6015018f987cf
Frank Vicino <fvicino@hotmail.com>
2016-05-27 15:03:17 +1000

    Corrected Respondent JSON

--- ashoka-survey-web/app/models/respondent.rb
+++ ashoka-survey-web/app/models/respondent.rb
@@ -1,6 +1,6 @@
 class Respondent < ActiveRecord::Base
   def serializable_hash(opt = nil)
     opt ||= {}
-    return {:id=> id, :identifiers => JSON.parse(respondent_json) }
+    return JSON.parse(respondent_json)
   end
 end
