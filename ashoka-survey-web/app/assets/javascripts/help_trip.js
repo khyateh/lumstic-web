7bd5d9972bf6293e309cf48ec7554bb93b01641b
Anand <anand@khyateh.com>
2016-06-27 12:18:39 +0530

    Added additional help text

--- ashoka-survey-web/app/assets/javascripts/help_trip.js
+++ ashoka-survey-web/app/assets/javascripts/help_trip.js
@@ -50,8 +50,51 @@ var survey_index = [
     header: "Incomplete Responses",
     content : "The count of all incomplete records on the mobile is shown here. This number should ideally be very low.",
     expose : true    
-  }
+  },
+  {
+    sel : ".organization_dashboards",
+    header: "Survey Overview",
+    content : "This provides an overview of productivity of users by date, number of complete and incomplete surveys per users, together with the last submitted dates.",
+    expose : true    
+  }  
 ];
+
+var report_help = [
+  {
+    sel : ".go_btn",
+    header: "View report",
+    content : "You can select a desired date range of values, and view the different charts according to the data type of the report.",
+    expose : true    
+  },
+  {
+    sel : ".download_excel",
+    header: "Download Excel",
+    content : "You can select a desired date range of values, and download the Excel file. Report generation is an intensive process and it will take some time to provide the zip file for download.",
+    expose : true    
+  }    
+  ];
+  
+var survey_build = [
+  {
+    sel : ".settingOptionsWrp",
+    header: "Use settings",
+    content : "You can mark a question as mandatory, or private or as an identifier using this option.",
+    expose : true    
+  },
+  {
+    sel : "#finalize",
+    header: "Publish Survey",
+    content : "Using this option will let you publish the survey. Many editing features will be restricted after the survey is published. Unless the survey is published, it is not available on the mobile device.",
+    expose : true    
+  },
+  {
+    sel : "#save",
+    header: "Save Survey",
+    content : "Using this option will let you let you save the survey as draft. All saved surveys can be found under the drafts menu.",
+    expose : true    
+  }  
+  ];
+
 
 function initTrip(help_text)
 {
@@ -74,9 +117,18 @@ function help(page){
     var help_text;
     if(page == "survey_list")
     {
-       help_text = survey_index;       
-      
-      initTrip(help_text).start();                    
+       help_text = survey_index;
     }
+    if(page == "report_help")
+    {
+      help_text = report_help;
+    }
+    if(page == "survey_build")
+    {
+      help_text = survey_build;
+    }
+    
+    if(help_text != "")
+      initTrip(help_text).start();     
 }
 	
