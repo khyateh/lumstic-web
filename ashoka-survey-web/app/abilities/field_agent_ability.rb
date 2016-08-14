fe3a3d50a368c9cad91c240ce82a3f4d9f36e2de
Anand <anand@khyateh.com>
2016-08-14 15:49:01 +0530

    Updated abilities and roles for supervisor and field_agent user roles

--- ashoka-survey-web/app/abilities/field_agent_ability.rb
+++ ashoka-survey-web/app/abilities/field_agent_ability.rb
@@ -2,18 +2,39 @@ class FieldAgentAbility < Ability
   def initialize(user_info)
     @user_info = user_info
 
-    can :read, Survey.shared(@user_info[:user_id]) #,  :survey_users => { :user_id => user_info[:user_id] }
-    can :read, Survey, :organization_id => @user_info[:org_id]
-    can :read, Survey.with_participating_organizations(@user_info[:org_id])
-    
+    #can :read, Survey.shared(@user_info[:user_id]) #,  :survey_users => { :user_id => user_info[:user_id] }
+    # can :read, Survey, :organization_id => @user_info[:org_id]
+    # can :read, Survey.with_participating_organizations(@user_info[:org_id])
+    can :create, Survey, :survey_users => { :user_id => user_info[:user_id] }
+    can :index, Survey, :survey_users => { :user_id => user_info[:user_id] }
+    can :read, Survey.shared(@user_info[:user_id]) 
     can :report, Survey.shared(@user_info[:user_id]) # SurveyUser , :survey_users => { :user_id => user_info[:user_id] } #Survey, surveys_published_to_me
     can :generate_excel, Survey.shared(user_info[:user_id]) #Survey, surveys_published_to_me
-    can :create, Response #, :survey => { :survey_users => { :user_id => user_info[:user_id] } }
-    can :manage, Response #, :user_id => user_info[:user_id]
+    can_perform_on_responses_of_surveys_published_to_me(:manage)  
+    
+    #can :index, Response, :survey => {:survey_users => {:user_id => @user_info[:user_id]}}
+    #can :create, Response, :survey => {:survey_users => {:user_id => @user_info[:user_id]}} #, :organization_id => { :user_id => user_info[:org_id] }    
+    #can :create, Response #, :survey => { :survey_users => { :user_id => user_info[:user_id] } }
+    #can :destroy, Response,  :user_id => user_info[:user_id]
+    #can :manage, Response,  :user_id => user_info[:user_id]
+    
     cannot :destroy, Response
     cannot :provide_state, Response
-    can :read, Survey, :organization_id => user_info[:org_id]  
+    # can :read, Survey, :organization_id => user_info[:org_id]  
     
   end  
  
+ def can_perform_on_responses_of_surveys_published_to_me(action)
+    can action, Response, sql_for_responses_of_surveys_published_to_me do |response|
+      SurveyUser.find_by_user_id_and_survey_id(@user_info[:user_id], response.survey_id).present?
+    end
+  end
+
+  def sql_for_responses_of_surveys_published_to_me
+    [
+        'responses.survey_id IN (SELECT survey_users.survey_id from survey_users
+         WHERE survey_users.user_id = ?)', @user_info[:user_id]
+    ]
+  end
+  
 end
--- ashoka-survey-web/app/abilities/supervisor_ability.rb
+++ ashoka-survey-web/app/abilities/supervisor_ability.rb
@@ -2,15 +2,18 @@ class SupervisorAbility < Ability
   def initialize(user_info)
     @user_info = user_info
 
-    can :read, Survey.shared(@user_info[:user_id]) #,  :survey_users => { :user_id => user_info[:user_id] }
-    can :read, Survey, :organization_id => @user_info[:org_id]
-    can :read, Survey.with_participating_organizations(@user_info[:org_id])
-    can :report, Survey.shared(@user_info[:user_id]) # SurveyUser , :survey_users => { :user_id => user_info[:user_id] } #Survey, surveys_published_to_me
-    can :archive, Survey.shared(@user_info[:user_id])  #, :survey_users => { :user_id => user_info[:user_id] } 
-    can :generate_excel, Survey.shared(user_info[:user_id]) #Survey, surveys_published_to_me
-    can :view_survey_dashboard, Survey.shared(@user_info[:user_id]) # SurveyUser , :survey_users => { :user_id => user_info[:user_id] }#Survey, surveys_published_to_me
+       
+    can :index, Survey, :survey_users => { :user_id => user_info[:user_id] }
+    can :read, Survey.shared(@user_info[:user_id])
+    can :report, Survey #, :survey_users => { :user_id => user_info[:user_id] } 
+    #Survey.shared(@user_info[:user_id]) 
+    can :archive, Survey.shared(@user_info[:user_id]) 
+    can :generate_excel, Survey.shared(user_info[:user_id])
+    can :view_survey_dashboard, Survey.shared(@user_info[:user_id])
 
     can_perform_on_responses_of_surveys_published_to_me(:manage)
+    can :edit_publication, Survey.shared(@user_info[:user_id]) , :organization_id => user_info[:org_id] 
+    can :update_publication, Survey.shared(@user_info[:user_id]) , :organization_id => user_info[:org_id] 
     
   end
   
